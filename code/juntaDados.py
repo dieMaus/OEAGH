33957fad9b76983c9647e27414727ccb5d074f26
dieMaus <riquerosario@hotmail.com>
2016-06-16 22:20:19 -0300

    jesus man

--- code/juntaDados.py
+++ code/juntaDados.py
@@ -7,116 +7,127 @@
 import os
 import csv
 
-fieldnames = ['name', 'capital', 'largestCity', 'iso3166', 'internetTLD', 'callingCode',
-              'population', 'area', 'currencyName', 'currency3letter', 'gdp', 'idh', 'gini']
+fieldnames = ['name', 'capital', 'largestCity',
+              'iso3166-a3', 'iso3166-a2', 'internetTLD', 'callingCode',
+              'population', 'area', 'currencyName',
+              'currency3letter', 'gdp', 'idh', 'gini',
+              'lifeExp', 'continent', 'coastArea',
+              'govForm', 'currentInflation']
 
-final = open(os.path.abspath('../country_data/fullData.csv'), 'w+')
+# na minha matriz vai ficar assim
+# ['name', 'capital', 'largestCity','iso3166-a3', 'iso3166-a2', 'internetTLD', 'callingCode','population', 'area', 'currencyName','currency3letter', 'gdp', 'idh', 'gini']
+# ['paraguay','assuncion','etc',...
+#
+# vou alterando os dados incompletos da matriz enquanto leio todos os arquivos csv que eu tenho
+# e no final escrevo essa matriz mestre no arquivo csv final
 
-writer = csv.DictWriter(final, fieldnames=fieldnames, quoting=csv.QUOTE_ALL)
-writer.writeheader()
-readerPai = csv.DictReader(final)
+
+final = open(os.path.abspath('../country_data/fullData.csv'), 'w+', newline='')
+
+writer = csv.writer(final, quoting=csv.QUOTE_ALL)
+
+master = []
+master.append(fieldnames)
 
 with open(os.path.abspath('../country_data/country-codes.csv'), newline='') as codes:
     reader = csv.DictReader(codes)
     for linha in reader:
-        if linha['is_independent'] == "yes":
-            writer.writerow({'name': linha['official_name'],
-                             'iso3166': linha['ISO3166-1-Alpha-3'],
-                             'callingCode': linha['Dial'],
-                             'currency3letter': linha['currency_alphabetic_code'],
-                             'currencyName': linha['currency_name']})
+        if linha['is_independent'] == 'Yes':
+            master.append([linha['official_name'], 'n.a.', 'n.a.',
+                           linha['ISO3166-1-Alpha-3'], linha['ISO3166-1-Alpha-2'], 'n.a.', linha['Dial'],
+                           'n.a.', 'n.a.', linha['currency_name'],
+                           linha['currency_alphabetic_code'], 'n.a.', 'n.a.', 'n.a.',
+                           'n.a.', 'n.a.', 'n.a.', 'n.a.', 'n.a.'])
 
-with open(os.path.abspath('../country_data/country-list.csv'), newline='') as capitals:
+with open(os.path.abspath('../country_data/capitals.csv'), newline='', encoding='utf-8') as capitals:
     reader = csv.DictReader(capitals)
     for linha in reader:
-        for linhaPai in readerPai:
-            if linha['country'] == linhaPai['name']:
+        for linhaPai in master:
+            if linha['Country code'] == linhaPai[4]:
+                linhaPai[1] = linha['City (en)']
                 break
         else:
             # país não esta na lista do arquivo principal
             continue
-        writer.writerow({'capital': linha['capital']})
+
+with open(os.path.abspath('../country_data/countries.csv'), newline='', encoding='utf-8') as general:
+    reader = csv.DictReader(general)
+    for linha in reader:
+        for linhaPai in master:
+            if linha['Country code'] == linhaPai[4]:
+                linhaPai[15] = linha['Continent']
+                linhaPai[7] = linha['Population']
+                linhaPai[8] = linha['Area']
+                linhaPai[16] = linha['Coastline']
+                linhaPai[17] = linha['Government form'].capitalize()
+                linhaPai[14] = linha['Life expectancy']
+                break
+        else:
+            # país não esta na lista do arquivo principal
+            continue
 
 with open(os.path.abspath('../country_data/GDP spam 1960-2015.csv'), newline='') as gdp:
     reader = csv.DictReader(gdp)
     for linha in reader:
-        for linhaPai in readerPai:
-            if linha['Country Name'] == linhaPai['name']:
+        for linhaPai in master:
+            if linha['Country Code'] == linhaPai[3]:
+                ano = 2015
+                while not linha[str(ano)]:
+                    ano -= 1
+                    if ano == 1960:
+                        break
+                else:
+                    linhaPai[11] = linha[str(ano)]
                 break
         else:
             # país não esta na lista do arquivo principal
             continue
-
-        ano = 2015
-        while not linha[str(ano)]:
-            ano -= 1
-            if ano == 1960:
-                break
-        else:
-            writer.writerow({'gdp': linha[str(ano)]})
 
 with open(os.path.abspath('../country_data/GINI spam 1960-2015.csv'), newline='') as gini:
     reader = csv.DictReader(gini)
     for linha in reader:
-        for linhaPai in readerPai:
-            if linha['Country Name'] == linhaPai['name']:
+        for linhaPai in master:
+            if linha['Country Code'] == linhaPai[3]:
+                ano = 2015
+                while not linha[str(ano)]:
+                    ano -= 1
+                    if ano == 1960:
+                        break
+                else:
+                    linhaPai[13] = linha[str(ano)]
                 break
         else:
             # país não esta na lista do arquivo principal
             continue
-
-        ano = 2015
-        while not linha[str(ano)]:
-            ano -= 1
-            if ano == 1960:
-                break
-        else:
-            writer.writerow({'gini': linha[str(ano)]})
-
-with open(os.path.abspath('../country_data/POP total spam 1980-2010 .csv'), newline='') as pop:
-    reader = csv.DictReader(pop)
-    for linha in reader:
-        for linhaPai in readerPai:
-            if linha['Country Name'] == linhaPai['name']:
-                break
-        else:
-            # país não esta na lista do arquivo principal
-            continue
-
-        ano = 2015
-        while not linha[str(ano)]:
-            ano -= 1
-            if ano == 1960:
-                break
-        else:
-            writer.writerow({'population': linha[str(ano)]})
-
-with open(os.path.abspath('../country_data/surfaceArea spam 1960-2015.csv'), newline='') as area:
-    reader = csv.DictReader(area)
-    for linha in reader:
-        for linhaPai in readerPai:
-            if linha['Country Name'] == linhaPai['name']:
-                break
-        else:
-            # país não esta na lista do arquivo principal
-            continue
-
-        ano = 2015
-        while not linha[str(ano)]:
-            ano -= 1
-            if ano == 1960:
-                break
-        else:
-            writer.writerow({'area': linha[str(ano)]})
 
 with open(os.path.abspath('../country_data/IDH 2014.csv'), newline='') as idh:
     reader = csv.DictReader(idh)
     for linha in reader:
-        for linhaPai in readerPai:
-            if linha['country'] == linhaPai['Country Name']:
+        for linhaPai in master:
+            if linha['Country'] == linhaPai[0]:
+                linhaPai[12] = linha['2014']
                 break
         else:
             # país não esta na lista do arquivo principal
             continue
 
-        writer.writerow({'idh': linha['2014']})
+with open(os.path.abspath('../country_data/Inflacao data spam 1980-2015.csv'), newline='') as inflacao:
+    reader = csv.DictReader(inflacao)
+    for linha in reader:
+        for linhaPai in master:
+            if linha['Country Code'] == linhaPai[3]:
+                ano = 2015
+                while not linha[str(ano)]:
+                    ano -= 1
+                    if ano == 1960:
+                        break
+                else:
+                    linhaPai[18] = linha[str(ano)]
+                break
+        else:
+            # país não esta na lista do arquivo principal
+            continue
+
+writer.writerows(master)
+
+final.close()
