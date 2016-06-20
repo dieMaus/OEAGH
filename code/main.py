30ac07f54251240f58c1b61caf6a05dd2b8bbfb3
Germano <gtmartinelli@gmail.com>
2016-06-20 19:56:11 -0300

    comment

--- code/main.py
+++ code/main.py
@@ -1,6 +1,6 @@
 import gui_improved as gui
 
-# Essas tres linhas de codigo sao o resultado de todo nosso trabalho! :)
+# Essas tres linhas de codigo sao o derradeiro resultado de todo nosso trabalho! :)
 app = gui.App()
 app.title("Remove Kebab")
 app.mainloop()
