9b7e35ad44b02a649196f939034c9359520afc41
Germano <gtmartinelli@gmail.com>
2016-06-20 17:46:26 -0300

    limpando

--- code/objPais.py
+++ code/objPais.py
@@ -2,16 +2,14 @@
 
 
 class Country:
-    ## FUNCAO QUE INICIALIZA UM OBJETO COUNTRY
-    # Alterei todas as ocorrencias de "iso3116" por "iso3166" #Lauren
-    # NAO FORAM TODAS AS OCORRENCIAS ACHEI UMA Q AINDA TAVA COMO 3116 HAA
+    ## FUNÇÃO QUE INICIALIZA UM OBJETO COUNTRY
     def __init__(self, name, capital, iso3166a2, iso3166a3, internetTLD,
                  callingCode, population, area, currencyName,
                  currency3letter, gdp, hdi, gini, lifeExp, continent,
                  coastArea, govForm, currInflation, tourism, currencyDolar):
-        # area sera em km^2, GDP sera em milhoes de US$
+        # area é em km², GDP é em US$
 
-        # Dados basicos
+        # Dados básicos
         self.name = name
         self.capital = capital
 
@@ -41,7 +39,7 @@ class Country:
         self.tourism = tourism
         self.currencyExchange = currencyDolar
 
-    ## GET-METHODS (em orientacao a objeto, eh boa praxe usar esses metodos para acessar os dados)
+    ## GET-METHODS (em orientacao a objeto, é boa praxe usar esses métodos para acessar os dados)
 
     def getName(self):
         return self.name
@@ -109,17 +107,15 @@ class Country:
         self.popDensity = float(self.getPopulation()) / self.getArea()
 
     def get_popDensity(self):
-        # get-method para a variavel inicializada no metodo anterior
         return self.popDensity
 
     def compute_GDPperCapita(self):
         self.GDPperCapita = self.getGDP() / float(self.getPopulation())
 
     def get_GDPperCapita(self):
-        # get-method para a variavel inicializada no metodo anterior
         return self.GDPperCapita
 
-    # Adicionei esses dois metodos para criar o nome do arquivo de imagem #Lauren
+    # Criar o nome do arquivo de imagem
     def return_path_map(self):
         import os
         return os.path.abspath('../maps/' + self.iso3166a3 + '.gif')
@@ -128,42 +124,4 @@ class Country:
         import os
         return os.path.abspath('../flags/' + self.iso3166a3 + '.gif')
 
-    # definir metodos para imprimir dados (para testes e quica para uma caixa de texto na versao final)
-    # Para a caixa de texto, essa função não é necessária. Na versão final, proponho tirarmos esse método #Lauren
-
-    # pois é, mas enquanto n tivermos com tudo pronto, ela é bem util
-    # então, não tem problema deixar ela aqui, é só a gente n chamar ela #Henrique
-    def showCountryData(self):
-        self.compute_popDensity()
-        self.compute_GDPperCapita()
-
-        print("Country: ", self.getName())
-
-        print('\nCurrency: ', self.getCurrencyName(), "(" + self.getCurrencyCode() + ")")
-        print('\nISO-3116 alpha 2 Code: ', self.getIso3166a2())
-        print("\nISO-3116 alpha 3 Code: ", self.getIso3166a3())
-        print("\nInternet TLD: ", self.getInternetTLD())
-        print("\nCalling code: ", self.getCallingCode())
-
-        print("\n\nPopulation : ", self.getPopulation(), "inhabitants")
-        print("\nCapital:", self.getCapital())
-        print("\nArea: ", self.getArea(), "square kilometers")
-        print("\nCoastal area: ", self.getCoast(), "kilometers")
-        print("\nContinent: ", self.getContinent())
-        print("\nPopulation density: ", self.get_popDensity(), "inhabitants per square kilometer")
-        print("\nGovernment form: ", self.getGovernment())
-
-
-        print("\n\nGDP: ", self.getGDP(), "USD")
-        print("\nGDP per capita: ", self.get_GDPperCapita(), "USD per inhabitant")
-        print("\nCurrent inflation rate: ", self.getInflation(), "percent on year")
-        print("\nPercent of tourism on total exports: ", self.getTourism(), "percent")
-        print("\nCurrent exchange ratio over the US dolar: ", self.getCurrencyDolar(), "as 1US$/currency (as dolars)")
-
-        print("\n\nIDH: ", self.getIDH())
-        print("\nGini index: ", self.getGINI())
-        print("\nLife expectancy: ", self.getLifeExp(), 'years')
-
-
-# coalition1.showCountryData()
-# coalition2.showCountryData()
+   
